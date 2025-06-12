e29b07e204bfefd5976d3005b811026985eab312
haery123 <haerypun14@gmail.com>
2025-06-12 07:04:52 +0545

    css focused sidebar menu

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector(".sidebar-toggler").addEventListener("click", () => {
+// Toggle the class "collapsed" on the sidebar element
+// to show or hide the sidebar
+    document.querySelector(".sidebar").classList.toggle("collapsed");
+});
